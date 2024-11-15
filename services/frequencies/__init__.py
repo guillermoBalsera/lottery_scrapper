d9aac64b8fbee4d6e12a62c647f91004e80252c6
CasaJessica <guimodam@gmail.com>
2024-11-15 11:53:54 +0100

    Get last dates from lotteries

--- services/frequencies/__init__.py
+++ services/frequencies/__init__.py
@@ -1,6 +1,8 @@
 import json
 import os
 from collections import Counter
+from datetime import datetime
+from pathlib import Path
 
 FILES_PATH = './downloads'
 
@@ -14,22 +16,63 @@ def read_files():
     return data
 
 
+def get_lotteries(lottery_data):
+    return [lottery for year_list in lottery_data for lottery in year_list if lottery.get("numbers")]
+
+
+def get_numbers_and_stars(lotteries):
+    return ([number for lottery in lotteries for number in lottery.get("numbers")],
+            [stars for lottery in lotteries for stars in lottery.get("stars")])
+
+
+def get_number_results(lottery_numbers, lotteries_length):
+    return [{number: {"frequency": frequency, "percentage": round(frequency * 100 / lotteries_length, 2)}}
+            for number, frequency in sorted(Counter(lottery_numbers).items(), key=lambda x: x[1], reverse=True)]
+
+
+def get_star_results(lottery_stars, lotteries_length):
+    return [{star: {"frequency": frequency, "percentage": round(frequency * 100 / lotteries_length, 2)}}
+            for star, frequency in sorted(Counter(lottery_stars).items(), key=lambda x: x[1], reverse=True)]
+
+
+def get_last_dates(lotteries):
+    last_date = {}
+    for lottery in lotteries:
+        lottery_date = datetime.fromisoformat(lottery["date"])
+        for number in lottery["numbers"]:
+            if number not in last_date or lottery_date > last_date[number]:
+                last_date[number] = lottery_date
+
+    return [{"number": number, "last_date": date.strftime('%Y-%m-%d')} for number, date in last_date.items()]
+
+
+def write_results(number_results, star_results, most_probable_combination):
+    results = {
+        "numbers_stats": number_results,
+        "stars_stats": star_results,
+        "most_probable_combination": most_probable_combination
+    }
+    path_name = f"results/{datetime.now().strftime("%Y/%b/")}"
+    Path(path_name).mkdir(parents=True, exist_ok=True)
+    file_name = f"{path_name}/{datetime.now().strftime("%d")}.json"
+    with open(file_name, "w") as file:
+        json.dump(results, file, ensure_ascii=False, indent=4)
+
+
 def calculate():
     lottery_data = read_files()
-    lotteries = len([lottery for year_list in lottery_data for lottery in year_list if lottery.get("numbers")])
-    lottery_numbers = [number for year_list in lottery_data for lottery in year_list if lottery.get("numbers") for
-                       number in lottery.get("numbers")]
-    lottery_stars = [star for year_list in lottery_data for lottery in year_list if lottery.get("numbers") for star in
-                     lottery.get("stars")]
+    lotteries = get_lotteries(lottery_data)
 
-    numbers_result = [{number: {"frequency": frequency, "percentage": round(frequency * 100 / lotteries, 2)}} for
-                      number, frequency in sorted(Counter(lottery_numbers).items(), key=lambda x: x[1], reverse=True)]
-    stars_result = [{star: {"frequency": frequency, "percentage": round(frequency * 100 / lotteries, 2)}}
-                    for star, frequency in sorted(Counter(lottery_stars).items(), key=lambda x: x[1], reverse=True)]
+    last_dates = get_last_dates(lotteries)
+    lottery_numbers, lottery_stars = get_numbers_and_stars(lotteries)
+
+    numbers_result = get_number_results(lottery_numbers, len(lotteries))
+    stars_result = get_star_results(lottery_stars, len(lotteries))
 
     most_probable_combination = {
         "numbers": sorted([list(number.keys())[0] for number in numbers_result[:5]]),
         "stars": sorted([list(star.keys())[0] for star in stars_result[:2]])
     }
 
-    print(most_probable_combination)
+    write_results(numbers_result, stars_result, most_probable_combination)
+    print("most_probable_combination:", most_probable_combination)
